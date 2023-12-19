88e7f56c9f982b231dc15f2539bf4988ae4fc74d
root <root@40b0a8180a19>
2023-12-19 16:45:20 +0000

    2.100.0

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -27,7 +27,7 @@ variable "region" {
 variable "image" {
   type        = string
   description = "Cado Response VM image path"
-  default     = "projects/cado-public/global/images/cadoresponse-2-99-0"
+  default     = "projects/cado-public/global/images/cadoresponse-2-100-0"
 }
 
 variable "tags" {
